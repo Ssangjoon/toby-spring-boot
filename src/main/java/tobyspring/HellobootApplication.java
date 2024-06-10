5c6c0a86118e5b27b39197b3de3135b1b9f2ece6
SangJoon Lee <qkqh34k@gmail.com>
2024-06-10 23:29:29 +0900

    서블릿 등록

--- src/main/java/tobyspring/HellobootApplication.java
+++ src/main/java/tobyspring/HellobootApplication.java
@@ -7,11 +7,26 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		ServletWebServerFactory ServerFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = ServerFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type","text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+		});
 		webServer.start();
 	}
 
