1cee107223787085077cd49d11c19d112c930c7a
SangJoon Lee <qkqh34k@gmail.com>
2024-06-10 23:46:20 +0900

    프론트 컨트롤러 매핑과 바인딩

--- src/main/java/tobyspring/HelloController.java
+++ src/main/java/tobyspring/HelloController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RestController
 public class HelloController {
-
-    @GetMapping("/hello")
     public String hello(String name) {
-
         return "hello" + name;
     }
 }
--- src/main/java/tobyspring/HellobootApplication.java
+++ src/main/java/tobyspring/HellobootApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,14 +22,31 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		ServletWebServerFactory ServerFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = ServerFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			HelloController helloController = new HelloController();
+
+			// 서블릿을 추가하고
+			servletContext.addServlet("frontController", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					resp.setStatus(200);
-					resp.setHeader("Content-Type","text/plain");
-					resp.getWriter().println("Hello Servlet");
+					// 인증, 보안, 다국어, 공통 기능들을  처리한다.
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+						String name = req.getParameter("name");
+
+						// 복잡한 로직은 컨트롤러로 분리되었다.
+						String ret = helloController.hello(name);
+
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					}
+					else if(req.getRequestURI().equals("/user")){
+						//
+					}
+					else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
+				// 서블릿 컨테이너가 들어온 요청을 서블릿에게 연결하는 매핑 작업 해야 한다.
+			}).addMapping("/*");
 		});
 		webServer.start();
 	}
