34311e38354ee0869c228edf1f89aa30ae18f465
SangJoon Lee <qkqh34k@gmail.com>
2024-06-11 21:25:59 +0900

    스프링 컨테이너 통합 및 자바코드 구성정보 사용

--- src/main/java/tobyspring/HellobootApplication.java
+++ src/main/java/tobyspring/HellobootApplication.java
@@ -1,38 +1,41 @@
 package tobyspring;
 
-import org.apache.catalina.startup.Tomcat;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+@Configuration
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService){
+		return new HelloController(helloService);
+	}
+	@Bean
+	public HelloService helloService(){
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
-		applicationContext.refresh();
+		// 자바 코드를 이용한 구성 정보를 사용하려 AnnotationConfigWebApplicationContext 클래스로 컨테이너를 만들어야 한다.
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
 
-		ServletWebServerFactory ServerFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = ServerFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
-					.addMapping("/*");
-		});
-		webServer.start();
+				ServletWebServerFactory ServerFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = ServerFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this)
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
+		applicationContext.register(HellobootApplication.class);
+		applicationContext.refresh();
 	}
 }
